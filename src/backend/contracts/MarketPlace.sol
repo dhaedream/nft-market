466931b9143ddaf47cb540570c9190923c2ecc5c
Dhaesha M <dhaesha1@gmail.com>
2022-04-04 21:07:43 -0400

    assigning unchangable state variables + fee percentage constructor

--- src/backend/contracts/MarketPlace.sol
+++ src/backend/contracts/MarketPlace.sol
@@ -9,6 +9,15 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Marketplace is ERC721URIStorage {
-  
+    // Variables
+    //immutable means y can assign value only once.
+    address payable public immutable feeAccount; // the account that receives fees
+    uint public immutable feePercent; // the fee percentage on sales 
+    uint public itemCount; 
+    //fee constriuctor
+     constructor(uint _feePercent) {
+        feeAccount = payable(msg.sender);
+        feePercent = _feePercent;
+    }
 
 }
